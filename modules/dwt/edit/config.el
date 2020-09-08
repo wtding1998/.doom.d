71922980b3a5178ec0225fd3bf37ed390f3291b2
Wentao Ding <wtding1998@gmail.com>
2020-09-08 12:02:02 +0800

    add edit modules

---
+++ modules/dwt/edit/config.el
@@ -0,0 +1,40 @@
+;;; dwt/edit/config.el -*- lexical-binding: t; -*-
+
+;;; evil-snipe
+(use-package! evil-snipe
+  :config
+  (setq evil-snipe-scope 'visible)
+  (setq evil-snipe-repeat-scope 'visible)
+  (setq evil-snipe-spillover-scope 'buffer))
+
+;;; indent in elisp
+(add-hook 'emacs-lisp-mode-hook
+  (function (lambda ()
+          (setq evil-shift-width 2 tab-width 2))))
+
+
+;;; evil-multiedit
+;; Highlights all matches of the selection in the buffer.
+(use-package! evil-multiedit
+  :config
+(define-key evil-visual-state-map "R" 'evil-multiedit-match-all)
+
+;; Match the word under cursor (i.e. make it an edit region). Consecutive presses will
+;; incrementally add the next unmatched match.
+(define-key evil-normal-state-map (kbd "C-d") 'evil-multiedit-match-and-next)
+;; Match selected region.
+(define-key evil-visual-state-map (kbd "C-d") 'evil-multiedit-match-and-next)
+;; Insert marker at point
+(define-key evil-insert-state-map (kbd "C-d") 'evil-multiedit-toggle-marker-here)
+  ;; RET will toggle the region under the cursor
+  (define-key evil-multiedit-state-map (kbd "RET") 'evil-multiedit-toggle-or-restrict-region)
+
+  ;; ...and in visual mode, RET will disable all fields outside the selected region
+  (define-key evil-motion-state-map (kbd "RET") 'evil-multiedit-toggle-or-restrict-region)
+
+  ;; For moving between edit regions
+  (define-key evil-multiedit-state-map (kbd "C-n") 'evil-multiedit-next)
+  (define-key evil-multiedit-state-map (kbd "C-p") 'evil-multiedit-prev)
+  (define-key evil-multiedit-insert-state-map (kbd "C-n") 'evil-multiedit-next)
+  (define-key evil-multiedit-insert-state-map (kbd "C-p") 'evil-multiedit-prev)
+  )
---
+++ modules/dwt/edit/packages.el
@@ -0,0 +1,6 @@
+;; -*- no-byte-compile: t; -*-
+;;; dwt/edit/packages.el
+
+(package! symbol-overlay)
+(package! evil-multiedit)
+(package! evil-mark-replace)
