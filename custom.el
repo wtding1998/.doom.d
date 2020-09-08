5e97b2d9e69f46da117e8a43aafc7f0fca857398
Wentao Ding <wtding1998@gmail.com>
2020-09-08 22:17:00 +0800

    add org config

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("/mnt/d/OneDrive/Documents/diary/org/research.org" "/mnt/d/OneDrive/Documents/diary/org/agenda.org" "/mnt/d/OneDrive/Documents/study note/org/emacs_note.org" "/mnt/d/Onedrive/Documents/journal.org" "/mnt/d/Onedrive/Documents/notes.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ modules/dwt/org/config.el
@@ -0,0 +1,90 @@
+;;; dwt/org/config.el -*- lexical-binding: t; -*-
+
+
+;; === org-mode ===
+;; org-function
+(after! org
+  (setq org-use-fast-todo-selection t)
+  (setq org-todo-keywords '((sequence "TODO(t)" "Wait(w)" "|" "DONE(d)" "DONELOG(l@/!)" "ABORT(a@/!)")))
+  (setq org-log-done t)
+  (setq org-log-into-drawer t)
+  (setq org-enforce-todo-dependencies t)
+  (setq org-enforce-todo-checkbox-dependencies t)
+  (add-hook (quote org-mode-hook)
+          (lambda ()
+            (org-shifttab 2)))
+;; === templates ===
+  (setq org-capture-templates nil)
+  (add-to-list 'org-capture-templates
+               '("a" "Agenda"
+                 entry (file+olp "/mnt/d/OneDrive/Documents/diary/org/agenda.org" "Agenda")
+                 "* %t - %^{title} %^g\n%?\n"
+                 ))
+  ;; (add-to-list 'org-capture-templates
+  ;;              '("p" "Entertainment"
+  ;;                entry (file+olp "/mnt/d/OneDrive/Documents/diary/org/agenda.org" "Entertainment")
+  ;;                "* %t - %^{title} %^g\n %?"
+  ;;                ))
+  (add-to-list 'org-capture-templates
+               '("d" "Diary"
+                 entry (file+datetree "/mnt/d/OneDrive/Documents/diary/org/diary.org")
+                 "* %<%H-%M> - %^{heading} \n %?\n"
+                 ))
+  (add-to-list 'org-capture-templates '("e" "English"))
+  (add-to-list 'org-capture-templates
+               '("ew" "Words"
+                 entry (file+olp "/mnt/d/OneDrive/Documents/study note/org/English_note.org" "Words")
+                 "* %^{title} \n %?"
+                 ))
+  ;; interesting
+  (add-to-list 'org-capture-templates '("i" "Interesting"))
+  (add-to-list 'org-capture-templates
+               '("it" "Interesting Things"
+                 entry (file+datetree "/mnt/d/OneDrive/Documents/study note/org/interesting_things.org")
+                 "* %<%H-%M> - %^{heading} \n %?\n"
+                 ))
+  (add-to-list 'org-capture-templates
+               '("is" "Interesting Sentences"
+                 entry (file "/mnt/d/OneDrive/Documents/study note/org/interesting_sentencse.org")
+                 "* %^{Sentence} \n"
+                 ))
+  ;; notes
+  (add-to-list 'org-capture-templates '("n" "Notes"))
+  (add-to-list 'org-capture-templates
+               '("nm" "Math"
+                 entry (file+datetree "/mnt/d/OneDrive/Documents/study note/org/math_note.org")
+                 "* %^{heading} \n %?\n"
+                 ))
+  (add-to-list 'org-capture-templates
+               '("no" "Other Notes"
+                 entry (file "/mnt/d/OneDrive/Documents/study note/org/other_note.org")
+                 "* %^{heading} \n %?\n"
+                 ))
+  ;; readings
+  (add-to-list 'org-capture-templates '("r" "Reading"))
+  (add-to-list 'org-capture-templates
+               '("rm" "Mathematical Books " entry
+                 (file+olp "/mnt/d/OneDrive/Documents/study note/org/reading.org" "Mathematical Books")
+                 "* TODO %t %^{title}\n" ))
+  (add-to-list 'org-capture-templates
+               '("rl" "Machine Learning" entry
+                 (file+olp "/mnt/d/OneDrive/Documents/study note/org/reading.org" "Machine Learning")
+                 "* TODO %t %^{title}\n" ))
+  (add-to-list 'org-capture-templates
+               '("ro" "Other Books" entry
+                 (file+olp "/mnt/d/OneDrive/Documents/study note/org/reading.org" "Other Books")
+                 "* TODO %t %^{title}\n" ))
+  (add-to-list 'org-capture-templates
+               '("re" "Emacs" entry
+                 (file+olp "/mnt/d/OneDrive/Documents/study note/org/reading.org" "Emacs Materials")
+                 "* TODO %t %^{title}\n" ))
+  ;; === cancel bold font in header ===
+    (dolist (face '(org-level-1
+                  org-level-2 org-level-3
+                  org-level-4 org-level-5
+                  org-level-6 org-level-7
+                  org-level-8))
+      (set-face-attribute face nil :weight 'normal))
+    ;; set scale for latex-preview
+    (plist-put org-format-latex-options :scale 2.7)
+    )
