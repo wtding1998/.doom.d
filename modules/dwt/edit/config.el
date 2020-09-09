28509fe160b4cab5a0ae66a5e88704a841e416d1
Wentao Ding <wtding1998@gmail.com>
2020-09-09 14:10:51 +0800

    start evil-multiedit when start

--- modules/dwt/edit/config.el
+++ modules/dwt/edit/config.el
@@ -15,17 +15,21 @@
 
 ;;; evil-multiedit
 ;; Highlights all matches of the selection in the buffer.
-(use-package! evil-multiedit
-  :defer t
-  :config
-  (define-key evil-visual-state-map "R" 'evil-multiedit-match-all)
-  ;; Match the word under cursor (i.e. make it an edit region). Consecutive presses will
-  ;; incrementally add the next unmatched match.
-  (define-key evil-normal-state-map (kbd "C-d") 'evil-multiedit-match-and-next)
-  ;; Match selected region.
-  (define-key evil-visual-state-map (kbd "C-d") 'evil-multiedit-match-and-next)
-  ;; Insert marker at point
-  (define-key evil-insert-state-map (kbd "C-d") 'evil-multiedit-toggle-marker-here)
+;; (use-package! evil-multiedit
+;;   :defer t
+;;   :config
+;;   )
+
+(define-key evil-visual-state-map "R" 'evil-multiedit-match-all)
+;; Match the word under cursor (i.e. make it an edit region). Consecutive presses will
+;; incrementally add the next unmatched match.
+(define-key evil-normal-state-map (kbd "C-d") 'evil-multiedit-match-and-next)
+;; Match selected region.
+(define-key evil-visual-state-map (kbd "C-d") 'evil-multiedit-match-and-next)
+;; Insert marker at point
+(define-key evil-insert-state-map (kbd "C-d") 'evil-multiedit-toggle-marker-here)
+
+(after! evil-multiedit
   ;; RET will toggle the region under the cursor
   (define-key evil-multiedit-state-map (kbd "RET") 'evil-multiedit-toggle-or-restrict-region)
   ;; ...and in visual mode, RET will disable all fields outside the selected region
