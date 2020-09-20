b969d6406d97b9cf2c1b37c79088b92c75bf3565
Wentao Ding <wtding1998@gmail.com>
2020-09-20 16:56:40 +0800

    add dired support

---
+++ modules/dwt/dwt-dired/config.el
@@ -0,0 +1,14 @@
+;;; dwt/dwt-dired/config.el -*- lexical-binding: t; -*-
+
+;; https://github.com/ralesi/ranger.el
+;; (use-package! ranger
+;;   :config
+;;   (setq ranger-footer-delay 0.2)
+;;   (setq ranger-preview-delay 0.040)
+;;   (setq ranger-parent-depth 2)
+;;   (setq ranger-width-parents 0.12)
+;;   (setq ranger-max-parent-width 0.12)
+;;   (setq ranger-preview-file t)
+;;   (setq ranger-show-literal t)
+;;   (setq ranger-width-preview 0.55)
+;;   )
