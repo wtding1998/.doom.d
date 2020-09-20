287f14945d474b2e91565f0c24a7aaef437528e7
Wentao Ding <wtding1998@gmail.com>
2020-09-20 16:55:48 +0800

    add latex support

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cdlatex-math-modify-alist '((97 "\\mathbb" "" t nil nil)))
+ '(cdlatex-math-symbol-alist '((99 ("\\eta"))))
  '(org-agenda-files
    '("/mnt/d/OneDrive/Documents/diary/org/research.org" "/mnt/d/OneDrive/Documents/diary/org/agenda.org" "/mnt/d/OneDrive/Documents/study note/org/emacs_note.org" "/mnt/d/Onedrive/Documents/journal.org" "/mnt/d/Onedrive/Documents/notes.org")))
 (custom-set-faces
