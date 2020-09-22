e578c6d5caa5a0e4310e41e8deb029be725551b4
Wentao Ding <wtding1998@gmail.com>
2020-09-22 12:22:52 +0800

    add counsel-kill-ring

--- modules/dwt/dwt-edit/config.el
+++ modules/dwt/dwt-edit/config.el
@@ -12,6 +12,66 @@
   (function (lambda ()
           (setq evil-shift-width 2 tab-width 2))))
 
+;;; kill-ring
+(defun my-prepare-candidate-fit-into-screen (s)
+  (let* ((w (frame-width))
+         ;; display kill ring item in one line
+         (key (replace-regexp-in-string "[ \t]*[\n\r]+[ \t]*" "\\\\n" s)))
+    ;; strip the whitespace
+    (setq key (replace-regexp-in-string "^[ \t]+" "" key))
+    ;; fit to the minibuffer width
+    (if (> (length key) w)
+        (setq key (concat (substring key 0 (- w 4)) "...")))
+    (cons key s)))
+
+(defun my-select-from-kill-ring (fn)
+  "If N > 1, yank the Nth item in `kill-ring'.
+If N is nil, use `ivy-mode' to browse `kill-ring'."
+  (interactive "P")
+  (let* ((candidates (cl-remove-if
+                       (lambda (s)
+                         (or (< (length s) 5)
+                             (string-match-p "\\`[\n[:blank:]]+\\'" s)))
+                       (delete-dups kill-ring)))
+          (ivy-height (/ (frame-height) 2)))
+     (ivy-read "Browse `kill-ring':"
+               (mapcar #'my-prepare-candidate-fit-into-screen candidates)
+               :action fn)))
+
+(defun my-delete-selected-region ()
+  "Delete selected region."
+  (when (region-active-p)
+    (delete-region (region-beginning) (region-end))))
+
+(defun my-insert-str (str)
+  "Insert STR into current buffer."
+  ;; ivy8 or ivy9
+  (if (consp str) (setq str (cdr str)))
+  ;; evil-mode?
+  (if (and (functionp 'evil-normal-state-p)
+           (boundp 'evil-move-cursor-back)
+           (evil-normal-state-p)
+           (not (eolp))
+           (not (eobp)))
+      (forward-char))
+
+  (my-delete-selected-region)
+
+  ;; insert now
+  (insert str)
+  str)
+
+(defun counsel-browse-kill-ring (&optional n)
+  "If N > 1, assume just yank the Nth item in `kill-ring'.
+If N is nil, use `ivy-mode' to browse `kill-ring'."
+  (interactive "P")
+  (my-select-from-kill-ring (lambda (s)
+                              (let* ((plain-str (my-insert-str s))
+                                     (trimmed (string-trim plain-str)))
+                                (setq kill-ring (cl-delete-if
+                                                 `(lambda (e) (string= ,trimmed (string-trim e)))
+                                                 kill-ring))
+                                (kill-new plain-str)))))
 
 ;;; evil-multiedit
 ;; Highlights all matches of the selection in the buffer.
