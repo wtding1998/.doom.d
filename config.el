78907a7e5f6d2330e10c39c7ff7b18ba4602d7ed
Wentao Ding <wtding1998@gmail.com>
2020-10-24 23:48:58 +0800

    add latex snippet

--- config.el
+++ config.el
@@ -71,6 +71,7 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+
 ;; maxmize frame when start
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
