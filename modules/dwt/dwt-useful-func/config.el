6f8d8d36436bcae62f416870ab08e79143bd017b
Wentao Ding <wtding1998@gmail.com>
2020-11-12 15:03:02 +0800

    add useful function

---
+++ modules/dwt/dwt-useful-func/config.el
@@ -0,0 +1,25 @@
+;;; dwt/dwt-use/config.el -*- lexical-binding: t; -*-
+
+;;; TODO write the configuration of how to enter certain files quickly.
+
+(defun dwt/create-config-links (label link)
+  (insert label ": ")
+  (insert-button link
+                 'action (lambda (_) (find-file link))
+                 'follow-link t)
+  (insert "\n"))
+
+(defun dwt/quick-open-configuration ()
+  (interactive)
+  (let ((buf (get-buffer-create "*Config Links*"))
+        (configs '(("ZSH" . "~/.zshrc")
+                   ("Doom" . "~/.doom.d")
+                   ("Vanilia" . "~/.emacs.d.vanilia")
+                   ("Emacs" . "~/.emacs.d")
+                   )))
+    (with-current-buffer buf
+      (erase-buffer)
+      (insert "My own configuration file :\n")
+      (dolist (config configs)
+        (dwt/create-config-links (car config) (cdr config)))
+    (pop-to-buffer buf t))))
