a558818c72755c2b044b69ac44f90e209be7aa94
Wentao Ding <wtding1998@gmail.com>
2020-11-17 09:36:18 +0800

    add key bindings

--- modules/dwt/dwt-key-bindings/config.el
+++ modules/dwt/dwt-key-bindings/config.el
@@ -19,6 +19,8 @@
       :nvm "K" (kbd "5k")
       :nvm "W" (kbd "5w")
       :nvm "B" (kbd "5b")
+      :nv "8" (kbd "^")
+      :nv "9" (kbd "$")
       :nv "[q" (kbd "C-c C-c")
       :nv "[p" 'goto-last-change
       :nv "]p" 'goto-last-change-reverse
