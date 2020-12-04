ab375d3de8aef25b5e989ee79a65f7974c9c133b
Wentao Ding <wtding1998@gmail.com>
2020-12-04 14:44:52 +0800

    revise function

--- modules/dwt/dwt-latex/config.el
+++ modules/dwt/dwt-latex/config.el
@@ -14,7 +14,7 @@
               TeX-PDF-mode t)
 ;;; auctex preview scale
 (after! preview
-    (setq-default preview-scale 2.5))
+  (setq-default preview-scale 2.5))
 
 (use-package! cdlatex
   :defer t
@@ -25,75 +25,114 @@
   (add-to-list 'TeX-view-program-selection
                '(output-pdf "llpp"))
   (add-to-list 'TeX-view-program-list
-             '("llpp"
-               ("llpp "
-                (mode-io-correlate " ")
-                "%o")
-               "llpp"))
+               '("llpp"
+                 ("llpp "
+                  (mode-io-correlate " ")
+                  "%o")
+                 "llpp")))
 
   ;;;###autoload
-  (defun dwt/find-math-next()
+(defun dwt/find-math-next()
   "Goto the next math environment in tex buffer."
-    (interactive)
-    (while (texmathp)
-        (evil-forward-word-begin))
-    (while (not (texmathp))
-      (evil-forward-word-begin)))
+  (interactive)
+  (while (texmathp)
+    (evil-forward-word-begin))
+  (while (not (texmathp))
+    (evil-forward-word-begin)))
 
   ;;;###autoload
-  (defun dwt/find-math-prev()
+(defun dwt/find-math-prev()
   "Goto the last math environment in tex buffer."
-    (interactive)
-    (while (texmathp)
-        (evil-backward-word-begin))
-    (while (not (texmathp))
-      (evil-backward-word-begin)))
+  (interactive)
+  (while (texmathp)
+    (evil-backward-word-begin))
+  (while (not (texmathp))
+    (evil-backward-word-begin)))
 
-  (defun dwt/insert-dollar ()
-    (interactive)
-    (unless (texmathp)
-      (insert "$$")
-      (left-char)))
+(defun dwt/insert-dollar ()
+  (interactive)
+  (unless (texmathp)
+    (insert "$$")
+    (left-char)))
 
-  (defun dwt/insert-superscript ()
-    (interactive)
-    (insert "^{}")
-    (left-char))
+(defun dwt/insert-superscript ()
+  "if not in math environment, insert math and wrap the word connects with it"
+  (interactive)
+  (if (texmathp)
+      (progn
+        (insert "^{}")
+        (left-char))
+    (progn
+      (if (thing-at-point 'word)
+          (progn
+            (call-interactively #'backward-word)
+            (insert "$")
+            (call-interactively #'forward-word)
+            (insert "^{}$")
+            (left-char 2)
+            )
+        (insert "$^{}$")
+        (left-char 4)))))
 
-  (defun dwt/insert-transpose ()
-    (interactive)
-    (insert "^{T}"))
+(defun dwt/insert-subscript ()
+  "if not in math environment, insert math and wrap the word connects with it"
+  (interactive)
+  (if (texmathp)
+      (progn
+        (insert "_{}")
+        (left-char))
+    (progn
+      (if (thing-at-point 'word)
+          (progn
+            (call-interactively #'backward-word)
+            (insert "$")
+            (call-interactively #'forward-word)
+            (insert "_{}$")
+            (left-char 2)
+            )
+        (insert "$_{}$")
+        (left-char 4)))))
 
 
-  (defun dwt/insert-star ()
-    (interactive)
-    (insert "^{*}"))
+(defun dwt/insert-transpose ()
+  (interactive)
+  (when (texmathp)
+    (insert "^{T}")))
 
-  (map!
-   :map LaTeX-mode-map
-   :localleader
-   :desc "View" "v" #'TeX-view
-   :desc "Run" "c" #'TeX-command-run-all
-   :desc "Toggle TeX-Fold" "f" #'TeX-fold-mode
-   :desc "Preview Environment" "e" #'preview-environment
-   :desc "Preview Buffer" "b" #'preview-buffer
-   :desc "Preview at Point" "p" #'preview-at-point
-   :desc "Clean preview" "R" #'preview-clearout-buffer
-   :desc "Clean preview" "r" #'preview-clearout-at-point
-   ;; :desc "Command" "c" "TeX-command-master"
-   :desc "toc" "t" #'reftex-toc)
 
-   (add-hook 'LaTeX-mode-hook
-            (lambda ()
-              ; bind { and  } to next math and previous math
-              (define-key evil-normal-state-local-map (kbd "}") 'dwt/find-math-next)
-              (define-key evil-visual-state-local-map (kbd "}") 'dwt/find-math-next)
-              (define-key evil-normal-state-local-map (kbd "{") 'dwt/find-math-prev)
-              (define-key evil-visual-state-local-map (kbd "{") 'dwt/find-math-prev)
-              (define-key evil-insert-state-local-map (kbd "M-\\") 'dwt/insert-dollar)
-              (define-key evil-insert-state-local-map (kbd "M-[") 'dwt/insert-superscript)
-              (define-key evil-insert-state-local-map (kbd "M-]") 'dwt/insert-transpose)
-              (define-key evil-insert-state-local-map (kbd "M-8") 'dwt/insert-star))))
+(defun dwt/insert-star ()
+  (interactive)
+  (when (texmathp)
+    (insert "^{*}")))
+
+(map!
+ :map LaTeX-mode-map
+ :localleader
+ :desc "View" "v" #'TeX-view
+ :desc "Run" "c" #'TeX-command-run-all
+ :desc "Toggle TeX-Fold" "f" #'TeX-fold-mode
+ :desc "Preview Environment" "e" #'preview-environment
+ :desc "Preview Buffer" "b" #'preview-buffer
+ :desc "Preview at Point" "p" #'preview-at-point
+ :desc "Clean preview" "R" #'preview-clearout-buffer
+ :desc "Clean preview" "r" #'preview-clearout-at-point
+ :desc "Master" "m" #'TeX-command-master
+ ;; :desc "Command" "c" "TeX-command-master"
+ :desc "toc" "t" #'reftex-toc)
+
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+                                        ; bind { and  } to next math and previous math
+            (define-key evil-normal-state-local-map (kbd "}") 'dwt/find-math-next)
+            (define-key evil-visual-state-local-map (kbd "}") 'dwt/find-math-next)
+            (define-key evil-normal-state-local-map (kbd "{") 'dwt/find-math-prev)
+            (define-key evil-visual-state-local-map (kbd "{") 'dwt/find-math-prev)
+            (define-key evil-insert-state-local-map (kbd "M-\\") 'dwt/insert-dollar)
+            (define-key evil-insert-state-local-map (kbd "M-[") 'dwt/insert-superscript)
+            (define-key evil-insert-state-local-map (kbd "M--") 'dwt/insert-subscript)
+            ;; (define-key evil-insert-state-local-map (kbd "M-]") 'dwt/insert-transpose)
+            ;; (define-key evil-insert-state-local-map (kbd "M-8") 'dwt/insert-star)
+            ))
 ;; this setting failed
 ;; TODO: add synctex forward and backward
 ;; (after! tex
@@ -111,10 +150,10 @@
 (use-package! reftex-toc
   :defer t
   :config
-       (setq! reftex-toc-follow-mode t
-       reftex-toc-split-windows-horizontally t
-       reftex-toc-split-windows-fraction 0.25
-       reftex-toc-follow-mode t))
+  (setq! reftex-toc-follow-mode t
+         reftex-toc-split-windows-horizontally t
+         reftex-toc-split-windows-fraction 0.25
+         reftex-toc-follow-mode t))
 
 ;;; find next or previous math environment
 
