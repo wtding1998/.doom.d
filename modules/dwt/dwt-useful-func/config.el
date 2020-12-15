c145fd94b263f3bed4644b70213f352fca9693e3
Wentao Ding <wtding1998@gmail.com>
2020-12-15 13:34:53 +0800

    change open-configuration by ivy

--- modules/dwt/dwt-useful-func/config.el
+++ modules/dwt/dwt-useful-func/config.el
@@ -9,17 +9,28 @@
                  'follow-link t)
   (insert "\n"))
 
-(defun dwt/quick-open-configuration ()
+;; (defun dwt/quick-open-configuration ()
+;;   (interactive)
+;;   (let ((buf (get-buffer-create "*Config Links*"))
+;;         (configs '(("ZSH" . "~/.zshrc")
+;;                    ("Doom" . "~/.doom.d")
+;;                    ("Vanilia" . "~/.emacs.d.vanilia")
+;;                    ("Emacs" . "~/.emacs.d")
+;;                    )))
+;;     (with-current-buffer buf
+;;       (erase-buffer)
+;;       (insert "My own configuration file :\n")
+;;       (dolist (config configs)
+;;         (dwt/create-config-links (car config) (cdr config)))
+;;       (pop-to-buffer buf t))))
+
+;;;###autoload
+(defun dwt/ivy-open-configuration ()
   (interactive)
-  (let ((buf (get-buffer-create "*Config Links*"))
-        (configs '(("ZSH" . "~/.zshrc")
-                   ("Doom" . "~/.doom.d")
-                   ("Vanilia" . "~/.emacs.d.vanilia")
-                   ("Emacs" . "~/.emacs.d")
-                   )))
-    (with-current-buffer buf
-      (erase-buffer)
-      (insert "My own configuration file :\n")
-      (dolist (config configs)
-        (dwt/create-config-links (car config) (cdr config)))
-    (pop-to-buffer buf t))))
+  (let ((configs '("~/.zshrc"
+                   "~/.doom.d/"
+                   "~/.emacs.d.vanilia/init.el"
+                   "~/.emacs.d/"
+                   "~/.config")))
+    (ivy-read "Open Configs: " configs :action 'find-file)))
+(map! :leader :desc "Open configuration" "op" #'dwt/ivy-open-configuration)
