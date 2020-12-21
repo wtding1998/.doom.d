8ae38076c2c967a9bfd6425fdd332496f0d5f62a
Wentao Ding <wtding1998@gmail.com>
2020-12-21 21:24:12 +0800

    add modules for nox

---
+++ modules/dwt/dwt-nox/config.el
@@ -0,0 +1,23 @@
+;;; dwt/dwt-nox/config.el -*- lexical-binding: t; -*-
+
+(use-package! nox
+  :defer t
+  :config
+  (setq nox-python-path (executable-find "python3")
+        nox-python-server-dir "~/.emacs.d/nox/mspyls/")
+  (dolist (hook (list
+                 'js-mode-hook
+                 'rust-mode-hook
+                 'python-mode-hook
+                 'ruby-mode-hook
+                 'java-mode-hook
+                 'sh-mode-hook
+                 'php-mode-hook
+                 'c-mode-common-hook
+                 'c-mode-hook
+                 'c++-mode-hook
+                 'haskell-mode-hook))
+
+    (add-hook hook '(lambda () (nox-ensure))))
+  (add-hook 'python-mode-hook '(lambda () (remove-hook 'completion-at-point-functions 'python-completion-at-point t))))
+  ;; (eldoc-box-hover-at-point-mode)
---
+++ modules/dwt/dwt-nox/packages.el
@@ -0,0 +1,6 @@
+;; -*- no-byte-compile: t; -*-
+;;; dwt/dwt-nox/packages.el
+
+
+(package! nox :recipe (:host github :repo "manateelazycat/nox"))
+;; (package! eldoc-box)
