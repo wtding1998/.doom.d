ed3fdb6285430003312f88f370dd7735fc467dff
Wentao Ding <wtding1998@gmail.com>
2020-12-25 17:36:14 +0800

    add cdlatex modify alist

--- custom.el
+++ custom.el
@@ -3,8 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cdlatex-math-modify-alist '((97 "\\mathbb" "" t nil nil)))
- '(cdlatex-math-symbol-alist '((72 ("\\nabla^2"))))
+ '(ansi-color-names-vector
+   ["#1B2B34" "#e68183" "#87af87" "#d9bb80" "#89beba" "#e68183" "#87c095" "#e6d6ac"])
+ '(cdlatex-math-modify-alist '((58 "" ":" t nil nil)))
+ '(cdlatex-math-symbol-alist '((58 (":"))))
+ '(custom-safe-themes
+   '("79278310dd6cacf2d2f491063c4ab8b129fee2a498e4c25912ddaa6c3c5b621e" "6b80b5b0762a814c62ce858e9d72745a05dd5fc66f821a1c5023b4f2a76bc910" "d5a878172795c45441efcd84b20a14f553e7e96366a163f742b95d65a3f55d71" "3c2f28c6ba2ad7373ea4c43f28fcf2eed14818ec9f0659b1c97d4e89c99e091e" "e6ff132edb1bfa0645e2ba032c44ce94a3bd3c15e3929cdf6c049802cf059a2a" "b5fff23b86b3fd2dd2cc86aa3b27ee91513adaefeaa75adc8af35a45ffb6c499" "e1ef2d5b8091f4953fe17b4ca3dd143d476c106e221d92ded38614266cea3c8b" "6c9cbcdfd0e373dc30197c5059f79c25c07035ff5d0cc42aa045614d3919dab4" "74ba9ed7161a26bfe04580279b8cad163c00b802f54c574bfa5d924b99daa4b9" "71e5acf6053215f553036482f3340a5445aee364fb2e292c70d9175fb0cc8af7" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" "9efb2d10bfb38fe7cd4586afb3e644d082cbcdb7435f3d1e8dd9413cbe5e61fc" default))
  '(evil-surround-pairs-alist
    '((40 "( " . " )")
      (91 "[ " . " ]")
@@ -20,8 +24,38 @@
      (60 . evil-surround-read-tag)
      (102 . evil-surround-function)
      (36 "$" . "$")))
+ '(fci-rule-color "#7c6f64")
+ '(jdee-db-active-breakpoint-face-colors (cons "#0d1011" "#d9bb80"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#0d1011" "#87af87"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#0d1011" "#5b5b5b"))
+ '(objed-cursor-color "#e68183")
  '(org-agenda-files
-   '("/mnt/d/OneDrive/Documents/diary/org/tensor-optimization.org" "/mnt/d/OneDrive/Documents/study note/org/cuhksz.org" "/mnt/d/OneDrive/Documents/diary/org/research.org" "/mnt/d/OneDrive/Documents/diary/org/agenda.org" "/mnt/d/OneDrive/Documents/study note/org/emacs_note.org" "/mnt/d/Onedrive/Documents/journal.org" "/mnt/d/Onedrive/Documents/notes.org")))
+   '("/mnt/d/OneDrive/Documents/diary/org/tensor-optimization.org" "/mnt/d/OneDrive/Documents/study note/org/cuhksz.org" "/mnt/d/OneDrive/Documents/diary/org/research.org" "/mnt/d/OneDrive/Documents/diary/org/agenda.org" "/mnt/d/OneDrive/Documents/study note/org/emacs_note.org" "/mnt/d/Onedrive/Documents/journal.org" "/mnt/d/Onedrive/Documents/notes.org"))
+ '(pdf-view-midnight-colors (cons "#e6d6ac" "#2a2426"))
+ '(rustic-ansi-faces
+   ["#2a2426" "#e68183" "#87af87" "#d9bb80" "#89beba" "#e68183" "#87c095" "#e6d6ac"])
+ '(vc-annotate-background "#2a2426")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#87af87")
+    (cons 40 "#a2b384")
+    (cons 60 "#bdb682")
+    (cons 80 "#d9bb80")
+    (cons 100 "#dcb07e")
+    (cons 120 "#dfa57c")
+    (cons 140 "#e39b7b")
+    (cons 160 "#e3927d")
+    (cons 180 "#e48980")
+    (cons 200 "#e68183")
+    (cons 220 "#e68183")
+    (cons 240 "#e68183")
+    (cons 260 "#e68183")
+    (cons 280 "#c37779")
+    (cons 300 "#a06e6f")
+    (cons 320 "#7d6465")
+    (cons 340 "#7c6f64")
+    (cons 360 "#7c6f64")))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
