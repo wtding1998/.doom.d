9bff92eba027e1ca7d71579ec81f0a709a18cf88
Wentao Ding <wtding1998@gmail.com>
2021-01-13 12:24:53 +0800

    delete useless module and use lsp as python lsp

--- init.el
+++ init.el
@@ -27,8 +27,8 @@
 
        :ui
        ;;deft              ; notational velocity for Emacs
-       doom              ; what makes DOOM look the way it does
-       doom-dashboard    ; a nifty splash screen for Emacs
+       ;; doom              ; what makes DOOM look the way it does
+       ;; doom-dashboard    ; a nifty splash screen for Emacs
        ;; doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
@@ -46,8 +46,8 @@
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-       window-select     ; visually switch windows
-       workspaces        ; tab emulation, persistence & separate workspaces
+       ;; window-select     ; visually switch windows
+       ;; workspaces        ; tab emulation, persistence & separate workspaces
        ;;zen               ; distraction-free coding or writing
 
        :editor
@@ -85,6 +85,7 @@
 
        :tools
        ;;ansible
+       ;; biblio
        ;; debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
@@ -94,7 +95,7 @@
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
        ;; (lsp +eglot)
-       ;; lsp
+       lsp
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
@@ -151,12 +152,13 @@
        ;; (org +pretty +jupyter +roam)              ; organize your plain life in plain text
        ;; (org +pretty)              ; organize your plain life in plain text
        (org +roam)
+       ;; (org )
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       ;; (python +lsp +pyright)            ; beautiful is better than ugly
+       (python +lsp +pyright)            ; beautiful is better than ugly
        ;; (python +lsp)
-       (python)            ; beautiful is better than ugly
+       ;; (python)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
@@ -198,11 +200,13 @@
        dwt-org
        dwt-yasnippet
        dwt-vterm
-       ;; dwt-lsp
+       dwt-lsp
        dwt-nox
        dwt-wsl2
+       dwt-pdf
        dwt-python
        dwt-useful-func
        dwt-eaf
        dwt-dired
+       dwt-custom
        )
