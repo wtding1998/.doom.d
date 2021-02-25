fc36fdb988e1553fa6e5d8adec4662b2c139fdfb
Wentao Ding <wtding1998@gmail.com>
2021-02-25 22:44:36 +0800

    add key binding for pdf-view

--- modules/dwt/dwt-pdf/config.el
+++ modules/dwt/dwt-pdf/config.el
@@ -1,7 +1,11 @@
 ;;; dwt/dwt-pdf/config.el -*- lexical-binding: t; -*-
 (after! pdf-tools
   (map! :map pdf-view-mode-map :n "zu" #'wsl/open-in-default-program
-        :map pdf-view-mode-map :n "zU" #'dwt/open-by-zathura))
+        :map pdf-view-mode-map :n "zU" #'dwt/open-by-zathura
+        :map pdf-view-mode-map :n "J" #'pdf-view-next-page
+        :map pdf-view-mode-map :n "K" #'pdf-view-previous-page
+        :map pdf-view-mode-map :n "C-j" #'pdf-history-forward
+        :map pdf-view-mode-map :n "C-k" #'pdf-history-backward))
 
 ;;;###autoload
 (defun dwt/open-by-zathura ()
