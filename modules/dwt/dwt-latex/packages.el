38259a9ee43e6c48fbd8793c7f9255cd51797b77
wtding <wtding1998@gmail.com>
2021-07-21 09:50:20 +0800

    update

--- modules/dwt/dwt-latex/packages.el
+++ modules/dwt/dwt-latex/packages.el
@@ -1,7 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; dwt/latex/packages.el
 
-;; (package! latex-preview-pane :disable t)
+(package! latex-preview-pane :disable t)
 (package! cdlatex)
 (package! org-latex-impatient :recipe (:host github :repo "yangsheng6810/org-latex-impatient"))
 (package! emacscompanywords :recipe (:host github :repo "wushuaibuaa/emacscompanywords"))
