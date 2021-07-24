611b627e22a14e4990ca2c5c797406f786c45513
wtding <wtding1998@gmail.com>
2021-07-24 16:36:25 +0800

    add matlab-shell module

--- modules/dwt/dwt-matlab/config.el
+++ modules/dwt/dwt-matlab/config.el
@@ -2,14 +2,24 @@
 (use-package! matlab
   :load-path "~/.doom.d/matlab-emacs"
   :defer t
-  :hook (matlab-mode . display-line-numbers-mode)
+  :hook ((matlab-mode . display-line-numbers-mode)
+         (matlab-mode . hl-todo-mode))
   :init
   (add-to-list 'auto-mode-alist '("\\.m\\'" . matlab-mode))
   :commands (matlab-mode)
   :config
   (map! :map matlab-mode-map "C-<return>" nil)
-  (map! :map matlab-mode-map :localleader "l" :desc "mlint" #'mlint-minor-mode))
+  (map! :map matlab-mode-map :localleader "l" :desc "mlint" #'mlint-minor-mode
+                                          "s" :desc "shell" #'matlab-shell))
 
 (use-package! mlint
   :defer t
   :commands (mlint-minor-mode))
+
+(use-package matlab-shell
+  :defer t
+  :commands (matlab-shell)
+  :config
+  (set-popup-rules!
+    ;; '(("^\\*Python*" :side right :size 15 :select t)))
+    '(("^\\*MATLAB*" :size 15 :select t))))
