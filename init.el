7e66ede319f5a01e7f708dd35ef7507ee477aaca
wtding <wtding1998@gmail.com>
2021-07-30 18:56:35 +0800

    fix error in modules

--- init.el
+++ init.el
@@ -61,8 +61,8 @@
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
-       ;; (parinfer +rust)          ; turn lisp into python, sort of
-       (:if IS-LINUX (parinfer + rust))
+        ;; (parinfer +rust)          ; turn lisp into python, sort of
+       (:if IS-LINUX (parinfer +rust))
        ;;rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
        ;; word-wrap         ; soft wrapping with language-aware indent
