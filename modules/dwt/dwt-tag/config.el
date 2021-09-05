abea0f1475cb9d788bde60b46b951c3559188d14
Wentao <wtding1998@gmail.com>
2021-09-05 18:11:01 +0800

    add tag file, although fail

---
+++ modules/dwt/dwt-tag/config.el
@@ -0,0 +1,6 @@
+;;; dwt/dwt-tag/config.el -*- lexical-binding: t; -*-
+
+(use-package! citre
+  :init
+  (setq citre-ctags-program "/opt/homebrew/Cellar/universal-ctags/HEAD-ca81123/bin/ctags")
+  (setq citre-readtags-program "/opt/homebrew/Cellar/universal-ctags/HEAD-ca81123/bin/readtags"))
---
+++ modules/dwt/dwt-tag/packages.el
@@ -0,0 +1,4 @@
+;; -*- no-byte-compile: t; -*-
+;;; dwt/dwt-tag/packages.el
+
+(package! citre :recipe (:host github :repo "universal-ctags/citre"))
