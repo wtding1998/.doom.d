b6b067fdff5555379bea40360732f709e8d7ebee
Wentao <wtding1998@gmail.com>
2023-03-03 14:36:13 +0800

    try eaf, failed

--- modules/dwt/dwt-eaf/config.el
+++ modules/dwt/dwt-eaf/config.el
@@ -1,21 +1,23 @@
 ;;; dwt/dwt-eaf/config.el -*- lexical-binding: t; -*-
 
 (use-package! eaf
-  :load-path "~/.doom.d/emacs-application-framework"; Set to "/usr/share/emacs/site-lisp/eaf" if installed from AUR
-  :defer t
-  :init
-  (map! :leader "oE" #'eaf-open)
-  :custom
-  (eaf-find-alternate-file-in-dired t)
+  :load-path "~/.emacs.d/.local/straight/repos/emacs-application-framework"
   :config
-  (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
-  (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
-  (eaf-bind-key take_photo "p" eaf-camera-keybinding)
-  ;; terminal
-  (eaf-setq eaf-terminal-font-size "22")
-  (eaf-setq eaf-terminal-font-family "SF MONO")
-  (eaf-setq eaf-jupyter-font-size "22")
-  (eaf-setq eaf-jupyter-font-family "SF MONO")
-  (eaf-setq eaf-browser-default-zoom "2")
-  ;; switch insert mode
-  (add-to-list 'evil-insert-state-modes 'eaf-mode))
+  (require 'eaf-pdf-viewer)); Set to "/usr/share/emacs/site-lisp/eaf" if installed from AUR
+  ;; :defer t
+  ;; :init
+  ;; (map! :leader "oE" #'eaf-open)
+  ;; :custom
+  ;; (eaf-find-alternate-file-in-dired t)
+  ;; :config
+  ;; (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
+  ;; (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
+  ;; (eaf-bind-key take_photo "p" eaf-camera-keybinding)
+  ;; ;; terminal
+  ;; (eaf-setq eaf-terminal-font-size "22")
+  ;; (eaf-setq eaf-terminal-font-family "SF MONO")
+  ;; (eaf-setq eaf-jupyter-font-size "22")
+  ;; (eaf-setq eaf-jupyter-font-family "SF MONO")
+  ;; (eaf-setq eaf-browser-default-zoom "2")
+  ;; ;; switch insert mode
+  ;; (add-to-list 'evil-insert-state-modes 'eaf-mode))
--- modules/dwt/dwt-eaf/packages.el
+++ modules/dwt/dwt-eaf/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; dwt/dwt-eaf/packages.el
 
-;; (package! eaf :recipe (:host github :repo "manateelazycat/emacs-application-framework"))
+(package! eaf :recipe (:host github :repo "manateelazycat/emacs-application-framework" :build (:not compile)))
 ;; eaf is not available using stright. Just clone it and use load-path in use-package is enough
