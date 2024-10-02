5d232e861dc291aa2eb98f364ee14988d34d2cd1
Wentao <wtding1998@gmail.com>
2024-10-02 23:19:44 +0800

    update matlab repo

--- modules/dwt/dwt-matlab/packages.el
+++ modules/dwt/dwt-matlab/packages.el
@@ -1,4 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; dwt/dwt-matlab/packages.el
 
-(package! matlab-emacs :recipe (:host nil :repo "https://git.code.sf.net/p/matlab-emacs/src"))
+(package!  Emacs-MATLAB-mode :recipe (:host github :repo "MathWorks/Emacs-MATLAB-Mode"))
