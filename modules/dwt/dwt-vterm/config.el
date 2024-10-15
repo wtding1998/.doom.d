96263ec370f183133459567b2d86de26fb40041f
Wentao Ding <wtding1998@gmail.com>
2024-10-15 23:07:13 +0800

    update

--- modules/dwt/dwt-vterm/config.el
+++ modules/dwt/dwt-vterm/config.el
@@ -5,7 +5,7 @@
   (map! :leader "oT" #'+macos/open-in-iterm))
 (map! :leader "ot" #'+vterm/here)
 (after! vterm
-  (defun dwt/vterm-toggle-current-dir ()
+  (defun dwt/vterm-send-current-dir ()
     "Insert cd path to the current buffer besides vterm"
     (interactive)
     (when (derived-mode-p 'vterm-mode)
@@ -13,11 +13,12 @@
              (current-window (nth 1 current-window-list))
              (current-buffer-besides-vterm (window-buffer current-window))
              (current-dir (file-name-directory (buffer-file-name current-buffer-besides-vterm))))
-        (vterm-send-string "cd ")
-        (vterm-send-string current-dir))))
-        
+        ;; (vterm-send-string "cd ")
+        (vterm-send-string current-dir)
+        (call-interactively #'evil-insert))))
+
   (map! :map vterm-mode-map
-        :n "q" #'dwt/vterm-toggle-current-dir)
+        :n "q" #'dwt/vterm-send-current-dir)
   (setq vterm-buffer-name-string "vterm %s")
   ;; disable the alt+num in vterm
   (map! :map vterm-mode-map
@@ -40,8 +41,8 @@
         :map vterm-mode-map :i "C-t" #'(lambda () (interactive) (vterm-send "C-t"))
         :map vterm-mode-map :i "C-d" #'(lambda () (interactive) (vterm-send "C-d"))
         ;; :map vterm-mode-map :i "C-v" #'(lambda () (interactive) (vterm-yank) (vterm-send-backspace))
-        :map vterm-mode-map :i "C-v" #'vterm-yank
-        :map vterm-mode-map :i "M-v" #'vterm-yank
-        :map vterm-mode-map :n "p"   #'(lambda () (interactive) (vterm-yank))))
+        ;; :map vterm-mode-map :i "C-v" #'vterm-yank
+        :map vterm-mode-map :i "C-M-v" #'vterm-yank
+        :map vterm-mode-map :n "p"   #'vterm-yank))
   ;; (set-popup-rules!
   ;;   '(("^\\*doom:vterm" :size 15 :select t)))
