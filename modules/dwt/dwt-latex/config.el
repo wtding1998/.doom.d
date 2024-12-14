958e21c719e88e6a445c98dce4cd632f0eec7d6e
Wentao Ding <wtding1998@gmail.com>
2024-12-14 20:14:00 +0800

    update

--- modules/dwt/dwt-latex/config.el
+++ modules/dwt/dwt-latex/config.el
@@ -95,9 +95,9 @@
                                    :help "Copy preamble"))
   (setq TeX-source-correlate-start-server t)
   (add-to-list 'TeX-view-program-selection '(output-pdf "PDF Tools"))
-  (add-to-list 'TeX-view-program-selection '(output-pdf "Sioyek"))
+  (add-to-list 'TeX-view-program-selection '(output-pdf "dwtSioyek"))
   ; deal with file name with space by add "" around %1
-  (add-to-list 'TeX-view-program-list '("Sioyek" "sioyek %o --forward-search-file \"%b\" --forward-search-line %n --inverse-search \"emacsclient +%2 \\\"%1\\\"\""))
+  (add-to-list 'TeX-view-program-list '("dwtSioyek" "sioyek %o --forward-search-file \"%b\" --forward-search-line %n --inverse-search \"emacsclient +%2 \\\"%1\\\"\""))
   (setq TeX-debug-warnings nil)
   ;; (setq TeX-debug-bad-boxes t)
   (run-with-idle-timer 10 t #'dwt/compile-latex-idle)
