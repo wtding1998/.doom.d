0a9a818315b2b73da461e1c136bee7836dc7c56c
Wentao Ding <wtding1998@gmail.com>
2025-04-19 22:46:04 +0800

    change highlightface

--- modules/dwt/dwt-ui/config.el
+++ modules/dwt/dwt-ui/config.el
@@ -66,7 +66,8 @@
           (load-theme dwt/gui-theme t nil)))
     ;; theme for TUI without daemon
     (load-theme dwt/tui-dark-theme t nil)
-    (copy-face 'button 'evil-ex-lazy-highlight)))
+    (custom-set-faces!
+      `(lazy-highlight :background ,(doom-color 'yellow)))))
 
 ;; hide title bar
 ;; (setq default-frame-alist '((undecorated . t)))
